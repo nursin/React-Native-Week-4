afd148c16a7b4c108bb6c94e20269f43117f5d00
Bobby Keel <bobbykeel76@gmail.com>
2021-08-09 22:42:51 -0400

    notifications api from expo sdk added to reservation component

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Text, View, ScrollView, StyleSheet, Picker, Switch, Button, Modal, Alert } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as Animatable from 'react-native-animatable';
+import * as Notifications from 'expo-notifications';
 
 class Reservation extends Component {
   constructor(props) {
@@ -38,7 +39,10 @@ class Reservation extends Component {
         },
         {
           test: 'OK',
-          onPress: () => this.resetForm()
+          onPress: () => {
+            this.presentLocalNotification(this.state.date.toLocaleDateString('en-US'));
+            this.resetForm();
+          }
         }
       ],
       { cancelable: false }
@@ -52,6 +56,31 @@ class Reservation extends Component {
       date: new Date(),
       showCalendar: false
     })
+  }
+
+  async presentLocalNotification(date) {
+    function sendNotification() {
+      Notifications.setNotificationHandler({
+        handleNotification: async () => ({
+          shouldShowAlert: true
+        })
+      });
+
+      Notifications.scheduleNotificationAsync({
+        content: {
+          title: 'Your Campsite Reservation Search',
+          body: `Search for ${date} requested`
+        },
+        trigger: null
+      });
+    }
+    let permissions = await Notifications.getPermissionsAsync();
+    if (!permissions.granted) {
+      permissions = await Notifications.requestPermissionsAsync();
+    }
+    if (permissions.granted) {
+      sendNotification();
+    }
   }
 
   render() {
